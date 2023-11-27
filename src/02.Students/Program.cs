af5929e45980a0b2d0e0f0b6f6d7c859f560a803
Tsveta Uzunova <137075660+tsveta-uzunova@users.noreply.github.com>
2023-11-27 14:24:54 +0200

    Add Students project.

---
+++ src/02.Students/Program.cs
@@ -0,0 +1,29 @@
+﻿using _01._Students;
+using System.Diagnostics;
+
+int numberStudent = int.Parse(Console.ReadLine());
+
+List<Student> students = new List<Student>();
+
+for (int i = 0; i < numberStudent; i++)
+{
+    string[] data = Console.ReadLine().Split(" ").ToArray();
+
+    Student student = new Student()
+    {
+        FirstName = data[0],
+        LastName = data[1],
+        Grade = double.Parse(data[2]),
+    };
+
+    students.Add(student);
+}
+
+students = students
+    .OrderByDescending(student => student.Grade)
+    .ToList();
+
+foreach (Student student in students)
+{
+    Console.WriteLine($"{student.FirstName} {student.LastName}: {student.Grade:F2}");
+}
---
+++ src/02.Students/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01._Students
+{
+    public class Student
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public double Grade { get; set; }
+    }
+}
