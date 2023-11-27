0d28c998dea69d20bfde81e3b515d77d035d53c4
Tsveta Uzunova <137075660+tsveta-uzunova@users.noreply.github.com>
2023-11-27 14:28:18 +0200

    Add Articles project.

---
+++ src/03.Articles/Program.cs
@@ -0,0 +1,38 @@
+﻿using _02._Articles;
+using System.Diagnostics;
+
+string articleDate = Console.ReadLine();
+
+string title = articleDate.Split(", ")[0];
+string content = articleDate.Split(", ")[1];
+string author = articleDate.Split(", ")[2];
+
+Article article = new Article(title, content, author);
+
+int numberOfCommands = int.Parse(Console.ReadLine());
+for (int i = 1; i <= numberOfCommands; i++)
+{
+    string command = Console.ReadLine();
+    string[] commandParts = command.Split(": ");
+    string commandName = commandParts[0];
+    string commandParameter = commandParts[1];
+
+    switch (commandName)
+    {
+        case "Edit":
+            article.Edit(commandParameter);
+             break;
+        case "ChangeAuthor":
+            article.ChangeAuthor(commandParameter);
+            break;
+        case "Rename":
+            article.Rename(commandParameter);
+            break;
+    }
+}
+
+Console.WriteLine(article.ToString());
+
+
+
+
